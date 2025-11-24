e395bedebf09f3947f31e1772a154d8471006347
edmand88 <edmand88@hotmail.com>
2025-11-24 11:38:06 -0500

    updated

--- extract_qwen_embeddings.py
+++ extract_qwen_embeddings.py
@@ -2,11 +2,8 @@ import torch
 import os
 from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLM
 
-save_dir = "/content"
-os.makedirs(save_dir, exist_ok=True)
-
 #should try other maybe larger models
-tokenizer_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+tokenizer_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-1.5B"
 filename_safe = tokenizer_name.replace("/", "_")
 model_name = tokenizer_name
 
@@ -20,16 +17,16 @@ print("Vocab size:", vocab_size)
 # Load the pre-trained model
 model = AutoModelForCausalLM.from_pretrained(model_name)
 
-embedding_dim = model.get_input_embeddings().embedding_dim
+embedding_dim = model.model.embed_tokens.weight.shape
 print("Embedding dimension:", embedding_dim)
 
 # extract embedding
-embeddings = model.get_input_embeddings()
+embeddings = model.model.embed_tokens.weight
 print(f"Extracted Embeddings Layer for {model_name}: {embeddings}")
-torch.save(embeddings.state_dict(), os.path.join(save_dir, f"{filename_safe}_embeddings_qwen.pth"))
+torch.save(embeddings.detach(), os.path.join(f"{filename_safe}_embeddings_qwen.pth"))
 
 # extract unembedding
 unembedding = model.lm_head.weight
 print(f"Extracted Unembeddings Layer for {model_name}: {unembedding}")
-torch.save(unembedding.detach().cpu(), os.path.join(save_dir, f"{filename_safe}_unembeddings_qwen.pth"))
+torch.save(unembedding.detach(), os.path.join(f"{filename_safe}_unembeddings_qwen.pth"))
 
