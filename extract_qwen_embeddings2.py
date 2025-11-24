76726e878b34def5ec9fb3095a1d0992c35c51ec
edmand88 <edmand88@hotmail.com>
2025-11-24 17:15:26 -0500

    updated or initial commits

--- extract_qwen_embeddings2.py
+++ extract_qwen_embeddings2.py
@@ -3,7 +3,7 @@ import os
 from transformers import AutoTokenizer, AutoModel, AutoModelForCausalLM
 
 #should try other maybe larger models
-tokenizer_name = "deepseek-ai/DeepSeek-R1-Distill-Qwen-7B"
+tokenizer_name = "Qwen/Qwen3-4B-Instruct-2507"
 filename_safe = tokenizer_name.replace("/", "_")
 model_name = tokenizer_name
 
@@ -14,7 +14,7 @@ vocab_size = tokenizer.vocab_size
 print("Vocab size:", vocab_size)
 
 # Load the pre-trained model
-model = AutoModelForCausalLM.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, torch_dtype="auto", device_map="auto")
 
 embedding_dim = model.model.embed_tokens.weight.shape
 print("Embedding dimension:", embedding_dim)
@@ -22,9 +22,8 @@ print("Embedding dimension:", embedding_dim)
 # extract embedding
 embeddings = model.model.embed_tokens.weight
 print(f"Extracted Embeddings Layer for {model_name}: {embeddings}")
-torch.save(embeddings.detach().cpu(), os.path.join(f"{filename_safe}_embeddings_qwen.pth"))
+torch.save(embeddings.detach(), os.path.join(f"{filename_safe}_embeddings_qwen.pth"))
 
-# extract unembedding
-unembedding = model.lm_head.weight
+unembedding = model.get_output_embeddings().weight
 print(f"Extracted Unembeddings Layer for {model_name}: {unembedding}")
-torch.save(unembedding.detach().cpu(), os.path.join(f"{filename_safe}_unembeddings_qwen.pth"))
+torch.save(unembedding.detach(), os.path.join(f"{filename_safe}_unembeddings_qwen.pth"))
